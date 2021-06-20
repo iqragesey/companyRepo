9a5bbc6a3b9b947c115533f561846a4fb81b272d
iqragesey <iqragesey@gmail.com>
2021-06-20 09:56:26 -0400

    final 3rd car at your service

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         car audi = new car();
+        audi.model = "A4";
+        audi.door = "2";
+        audi.number = "999";
+        audi.color = "white";
+        
+
 
     }
 
