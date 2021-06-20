dafa6916093b97fac7db645ff15879c72368201e
iqragesey <iqragesey@gmail.com>
2021-06-20 13:03:20 -0400

    latest commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import com.sun.codemodel.internal.JCodeModel;
+import sun.jvm.hotspot.code.ScopeDesc;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,10 +11,11 @@ public class Main {
         audi.door = "2";
         audi.number = "999";
         audi.color = "white";
-        
+
         test cases for the color of the car
         test cases for immediate requirement
         is being written down
+
 
 
         
